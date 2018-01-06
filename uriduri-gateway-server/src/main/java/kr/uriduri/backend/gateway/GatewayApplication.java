f044519cd74d89a28af01b18b880b5b05e522d8d
JangHee Han <zzanghi21@naver.com>
2018-01-06 21:35:51 +0900

    s

--- uriduri-gateway-server/src/main/java/kr/uriduri/backend/gateway/GatewayApplication.java
+++ uriduri-gateway-server/src/main/java/kr/uriduri/backend/gateway/GatewayApplication.java
@@ -2,12 +2,10 @@ package kr.uriduri.backend.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
+@EnableSidecar
 public class GatewayApplication {
 
 	public static void main(String[] args) {
